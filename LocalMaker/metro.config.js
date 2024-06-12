9e68686f55bdde8b4aa886f49449fe74deb6153b
jperez-2022076 <jperez-2022076@kinal.edu.gt>
2024-06-11 22:49:56 -0600

    HomePage funciona

--- LocalMaker/metro.config.js
+++
@@ -1,7 +0,0 @@
-// metro.config.js
-module.exports = {
-    resolver: {
-      sourceExts: ['jsx', 'js', 'ts', 'tsx'], // Agrega las extensiones que necesites
-    },
-  };
-  
--- README.md
+++
@@ -1,1 +0,0 @@
-# LocalMaker-ReactNative
